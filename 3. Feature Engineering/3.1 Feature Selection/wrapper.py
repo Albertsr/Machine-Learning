46d6baf5851e158e120a682e0498fc2fbffab815
Albert <maxiaoscut@aliyun.com>
2018-11-02 18:09:56 +0800

    Update wrapper.py

--- 3. Feature Engineering/3.1 Feature Selection/wrapper.py
+++ 3. Feature Engineering/3.1 Feature Selection/wrapper.py
@@ -2,7 +2,6 @@
 # E-mail：maxiaoscut@aliyun.com
 # Github：https://github.com/Albertsr
 
-
 import numpy as np
 import pandas as pd
 from sklearn.datasets import load_boston
@@ -25,14 +24,16 @@ sklearn.feature_selection模块提供了两个API可用于wrapper，分别为：
 2. RFECV_XGB.ranking_ ：数值型列表，若特征被保留则相应索引处为1，否则大于1，且ranking值越大，特征越不重要
 3. RFECV_XGB.grid_scores_ ：数值型列表，表示特征子集的交叉验证分数，与特征是否被选择没有太大关系
 
-
 '''
+
 X, y = load_boston(return_X_y=True)
 xgb = XGBRegressor(learning_rate=0.2, n_estimators=150, random_state=2017)
 RFECV_XGB = RFECV(xgb, cv=5, scoring='neg_mean_squared_error', n_jobs=-1)
 RFECV_XGB.fit(X, y)
 print('Original features: {:}'.format(X.shape[1]))
 print('RFECV_XGB features: {:}'.format(RFECV_XGB.n_features_))
+
+# 将RFECV_XGB的训练结果用pandas.dataframe进行直观展示
 feature_name = load_boston().feature_names
 rfecv_dict = {'Support':RFECV_XGB.support_, 'Ranking':RFECV_XGB.ranking_, 'Grid_scores':RFECV_XGB.grid_scores_}
 rfecv_result = pd.DataFrame(rfecv_dict, index=feature_name)
