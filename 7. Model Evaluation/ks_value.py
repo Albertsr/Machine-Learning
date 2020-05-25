72d58e7187e003aebc4df0ae5914120640c4b80c
Ma Xiao <maxiaoscut@aliyun.com>
2020-05-25 14:16:56 +0800

    Update ks_value.py

--- 7. Model Evaluation/ks_value.py
+++ 7. Model Evaluation/ks_value.py
@@ -7,7 +7,7 @@ import numpy as np
 from sklearn.metrics import confusion_matrix, make_scorer
 
 
-def ks(y_true, y_prob, thresholds_num=1000):
+def get_ks(y_true, y_prob, thresholds_num=500):
     # 生成一系列阈值
     thresholds = np.linspace(np.min(y_prob), np.max(y_prob), thresholds_num) 
     
@@ -19,8 +19,5 @@ def ks(y_true, y_prob, thresholds_num=1000):
         delta = tpr - fpr
         return delta
 
-    max_delta = np.max(list(map(tpr_fpr_delta, thresholds)))
+    max_delta = np.max([tpr_fpr_delta(threshold) for threshold in thresholds])
     return max_delta
-
-# ks_score可直接用于网格搜索scoring参数的取值
-ks_score = make_scorer(ks, greater_is_better=True, needs_proba=True) 
